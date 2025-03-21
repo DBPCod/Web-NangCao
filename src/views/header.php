efca06916b03d91715a614e85e01153ff5efa4f9
kaiwu24 <khoavu1831@gmail.com>
2025-03-21 10:44:50 +0700

    upd

---
+++ src/views/header.php
@@ -0,0 +1,38 @@
+<!-- Header -->
+<header class="header">
+    <div class="container">
+        <div class="row align-items-center">
+            <!-- Logo and Slogan -->
+            <div class="col-3">
+                <div class="logo-container">
+                    <img src="./public/img/logo.png" alt="logo" class="logo" />
+                    <span class="logo-text">SMART STATION</span>
+                </div>
+            </div>
+
+            <!-- Search Bar -->
+            <div class="col-6 d-flex justify-content-center">
+                <div class="input-group search-bar">
+                    <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Hôm nay bạn muốn tìm kiếm gì?" />
+                    <button class="btn btn-outline-secondary" type="button">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </div>
+            </div>
+
+            <!-- Icons -->
+            <div class="col-3 text-end">
+                <a href="#" class="icon-link">
+                    <i class="bi bi-person"></i> Tài khoản
+                </a>
+                <a href="#" class="icon-link cart-icon">
+                    <i class="bi bi-cart"></i>
+                    <span class="cart-badge">0</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
