5a4db5b373d07497216b174802aeac52eceb4fdd
kaiwu24 <khoavu1831@gmail.com>
2025-03-21 22:58:16 +0700

    change slider + style repo header

--- src/views/header.php
+++ src/views/header.php
@@ -1,9 +1,9 @@
 <!-- Header -->
 <header class="header">
     <div class="container">
-        <div class="row align-items-center">
+        <div class="row align-items-center flex-column flex-md-row">
             <!-- Logo and Slogan -->
-            <div class="col-3">
+            <div class="col-12 col-md-3 text-center text-md-start">
                 <div class="logo-container">
                     <img src="./src/public/img/logo.png" alt="logo" class="logo" />
                     <span class="logo-text">SMART STATION</span>
@@ -11,12 +11,9 @@
             </div>
 
             <!-- Search Bar -->
-            <div class="col-6 d-flex justify-content-center">
+            <div class="col-12 col-md-6 d-flex justify-content-center mt-2 mt-md-0">
                 <div class="input-group search-bar">
-                    <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Hôm nay bạn muốn tìm kiếm gì?" />
+                    <input type="text" class="form-control" placeholder="Hôm nay bạn muốn tìm kiếm gì?" />
                     <button class="btn btn-outline-secondary" type="button">
                         <i class="bi bi-search"></i>
                     </button>
@@ -24,9 +21,9 @@
             </div>
 
             <!-- Icons -->
-            <div class="col-3 text-end">
-                <a href="#" class="icon-link">
-                    <i class="bi bi-person"></i> Tài khoản
+            <div class="col-12 col-md-3 text-end text-md-end mt-2 mt-md-0">
+                <a href="#" class="icon-link account-icon">
+                    <i class="bi bi-person"></i> Đăng nhập
                 </a>
                 <a href="#" class="icon-link cart-icon">
                     <i class="bi bi-cart"></i>
