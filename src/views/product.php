300641cb01b51c19edee300836e0d21b3e496450
kaiwu24 <khoavu1831@gmail.com>
2025-03-22 20:45:00 +0700

    updt slider + sorting

--- src/views/product.php
+++ src/views/product.php
@@ -138,13 +138,12 @@
                 <!-- Sorting Options -->
                 <div class="d-flex justify-content-between mb-3">
                     <div>
-                        <button class="btn btn-outline-secondary">Sắp xếp theo</button>
-                        <button class="btn btn-outline-secondary">Liên quan</button>
+                        <span class="me-2" style="font-weight: 500; color:#333">Sắp xếp theo:</span>
                         <button class="btn btn-outline-secondary">Mới nhất</button>
                         <button class="btn btn-outline-secondary">Bán chạy</button>
                     </div>
                     <div>
-                        <button class="btn btn-outline-secondary">
+                        <button class="btn btn-outline-secondary" style="white-space: nowrap;">
                             Giá tiền <i class="bi bi-chevron-down"></i>
                         </button>
                     </div>
