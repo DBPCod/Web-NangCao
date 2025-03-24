ec2ce2abbedd17d816478b5694f9464a5b9eea77
dbp <dbp0932@gmail.com>
2025-03-24 17:10:27 +0700

    xoa file adminLogin.php va fix cònflict config.php

--- src/config.php
+++ src/config.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-    define("host_name","127.0.0.1:3307");
-=======
     define("host_name","localhost");
->>>>>>> 89f4cbc6307a5e56e42079f91a0463a8eef86175
     define("db_user","root");
     define("db_password","");
     define("db_name","ss");
