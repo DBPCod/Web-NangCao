236dcdf4aa8cba37fe0267eeda279eb0ffd1c4f7
dbp <dbp0932@gmail.com>
2025-03-24 15:39:10 +0700

    cap nhat api doi tuong sanpham

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("host_name","localhost");
-    define("db_user","root");
-    define("db_password","");
-    define("db_name","ss");
-?>
-
--- src/mvc/models/SanPhamModel.php
+++ src/mvc/models/SanPhamModel.php
@@ -22,21 +22,28 @@ class SanPhamModel {
 
     public function addProduct($data)
     {
-        $stmt = $this->db->prepare("INSERT into sanpham (IdCHSP, IdDongSanPham, SoLuong) values (?,?,?)");
-        $stmt->bind_param("isi", $data['IdCHSP'], $data['IdDongSanPham'], $data['SoLuong']);
+        $stmt = $this->db->prepare("INSERT into sanpham (IdCHSP, IdDongSanPham, SoLuong, TrangThai) values (?,?,?,?)");
+        $stmt->bind_param("isii", $data['IdCHSP'], $data['IdDongSanPham'], $data['SoLuong'], $data['TrangThai']);
         $stmt->execute();
         return $this->getProductById($data['IdCHSP'], $data['IdDongSanPham']);
     }
 
-    // public function updateProduct($id, $name, $price) {
-    //     $stmt = $this->db->prepare("UPDATE products SET name = ?, price = ? WHERE id = ?");
-    //     $stmt->bind_param("sdi", $name, $price, $id);
+    public function updateProduct($idCHSP, $idDSP, $soLuong, $trangThai) {
+        $stmt = $this->db->prepare("UPDATE sanpham SET SoLuong = ?, TrangThai = ? WHERE IdCHSP = ? and IdDongSanPham = ?");
+        $stmt->bind_param("iiis", $soLuong, $trangThai, $idCHSP, $idDSP);
+        return $stmt->execute();
+    }
+
+    // public function deleteProduct($idCHSP, $idDSP) {
+    //     $stmt = $this->db->prepare("DELETE FROM sanpham WHERE IdCHSP = ? and IdDongSanPham= ?");
+    //     $stmt->bind_param("is", $idCHSP, $idDSP);
     //     return $stmt->execute();
     // }
 
     public function deleteProduct($idCHSP, $idDSP) {
-        $stmt = $this->db->prepare("DELETE FROM sanpham WHERE IdCHSP = ? and IdDongSanPham= ?");
-        $stmt->bind_param("is", $idCHSP, $idDSP);
+        $stmt = $this->db->prepare("UPDATE sanpham SET TrangThai = ? Where IdCHSP= ? and IdDongSanPham = ?");
+        $TrangThai = 0;
+        $stmt->bind_param("iis", $TrangThai, $idCHSP, $idDSP);
         return $stmt->execute();
     }
 }
