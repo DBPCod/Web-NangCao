f192b3e741df410fcc9b463de41d58cbc2d5f613
dbp <dbp0932@gmail.com>
2025-04-09 17:55:49 +0700

    them chuc nang quan ly khuyen mai

--- src/mvc/models/CTKhuyenMaiModel.php
+++ src/mvc/models/CTKhuyenMaiModel.php
@@ -13,30 +13,24 @@ class CTKhuyenMaiModel {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getCTKhuyenMaiById($idKhuyenMai, $idCHSP, $idDSP) {
-        $stmt = $this->db->prepare("SELECT * FROM ctkhuyenmai WHERE IdKhuyenMai = ? AND IdCHSP = ? AND IdDongSanPham = ?");
-        $stmt->bind_param("iis", $idKhuyenMai, $idCHSP, $idDSP);
+    public function getCTKhuyenMaiById($idKhuyenMai, $idDongSanPham) {
+        $stmt = $this->db->prepare("SELECT * FROM ctkhuyenmai WHERE IdKhuyenMai = ? AND IdDongSanPham = ?");
+        $stmt->bind_param("ii", $idKhuyenMai, $idDongSanPham);
         $stmt->execute();
         return $stmt->get_result()->fetch_assoc();
     }
 
     public function addCTKhuyenMai($data) {
-        $stmt = $this->db->prepare("INSERT INTO ctkhuyenmai (IdKhuyenMai, IdCHSP, IdDongSanPham) VALUES (?, ?, ?)");
-        $stmt->bind_param("iis", $data['IdKhuyenMai'], $data['IdCHSP'], $data['IdDongSanPham']);
+        $stmt = $this->db->prepare("INSERT INTO ctkhuyenmai (IdKhuyenMai, IdDongSanPham) VALUES (?, ?)");
+        $stmt->bind_param("is", $data['IdKhuyenMai'], $data['IdDongSanPham']);
         $stmt->execute();
-        return $this->getCTKhuyenMaiById($data['IdKhuyenMai'], $data['IdCHSP'], $data['IdDongSanPham']);
+        return $this->getCTKhuyenMaiById($data['IdKhuyenMai'], $data['IdDongSanPham']);
     }
 
-    // public function updateCTKhuyenMai($idKhuyenMai, $idCHSP, $idDSP) {
-    //     $stmt = $this->db->prepare("UPDATE ctkhuyenmai SET IdCHSP = ?, IdDongSanPham = ? WHERE IdKhuyenMai = ? AND IdCHSP = ? AND IdDongSanPham = ?");
-    //     $stmt->bind_param("isiis", $idKhuyenMai, $idCHSP, $idDSP);
-    //     return $stmt->execute();
-    // }
-
-    public function deleteCTKhuyenMai($idKhuyenMai, $idCHSP, $idDSP) {
-        $stmt = $this->db->prepare("DELETE FROM ctkhuyenmai WHERE IdKhuyenMai = ? AND IdCHSP = ? AND IdDongSanPham = ?");
-        $stmt->bind_param("iis", $idKhuyenMai, $idCHSP, $idDSP);
+    public function deleteCTKhuyenMai($idKhuyenMai, $idDongSanPham) {
+        $stmt = $this->db->prepare("DELETE FROM ctkhuyenmai WHERE IdKhuyenMai = ? AND IdDongSanPham = ?");
+        $stmt->bind_param("ii", $idKhuyenMai, $idDongSanPham);
         return $stmt->execute();
     }
-} 
+}
 ?>
