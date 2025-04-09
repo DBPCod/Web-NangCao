04c183c3678c3a26dd63615d7b018a11d64fb715
dbp <dbp0932@gmail.com>
2025-04-09 09:21:36 +0700

    them tinh nang quan ly thuong hieu

--- src/mvc/views/admin/home.php
+++ src/mvc/views/admin/home.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartStation</title>
+    <!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap JS -->
+
     <link rel="shortcut icon" href="../../../public/img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="../../../public/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../../public/css/admin.css">
