ae88b48b3f2a4eb458edcf9d7f68019ff42b4a1b
DBPCod <dbp0932@gmail.com>
2025-05-04 13:41:17 +0700

    Delete src directory

--- src/mvc/views/user/sortPrice.php
+++
@@ -1,7 +0,0 @@
-<div class="d-flex justify-content-between mb-3">
-    <div>
-        <span class="me-2" style="font-weight: 500; color:#333">Sắp xếp theo:</span>
-        <button class="btn btn-outline-secondary" onclick="filterNewProducts()">Mới nhất</button>
-        <button class="btn btn-outline-secondary" onclick="filterBestSellingProducts()">Bán chạy</button>
-    </div>
-</div>
